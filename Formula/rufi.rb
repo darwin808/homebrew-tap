d8f62645f7b597edf08683bbbe5b049fb79de79c
Darwin ❤️❤️❤️ <71545960+darwin808@users.noreply.github.com>
2025-12-29 12:48:39 +0800

    Update Rufi to version 0.3.0

--- Formula/rufi.rb
+++ Formula/rufi.rb
@@ -1,13 +1,17 @@
 class Rufi < Formula
     desc "A fast application launcher for macOS"
     homepage "https://github.com/darwin808/rufi"
-    url "https://github.com/darwin808/rufi/releases/download/v0.2.0/rufi-macos.tar.gz"
-    sha256 "8ed6b15bd127d9dba1b5e5cdec805d70ca212571dfaff5c56a2ab24caedfede7"
+    url "https://github.com/darwin808/rufi/releases/download/v0.3.0/rufi-macos.tar.gz"
+    sha256 "4838f8917e1908edb9e55e8bab630bea60cbf8cad678d3c0fd75b5ac2d482a19"
     license "MIT"
 
     depends_on :macos
 
     def install
       bin.install "rufi"
+    end
+
+    test do
+      assert_match "rufi", shell_output("#{bin}/rufi --help")
     end
   end
