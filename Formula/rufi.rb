428e01a32a60eb157df0dfcc076e82f314408e7f
darwin808 <darwin.apolinario12@gmail.com>
2026-01-01 02:01:43 +0800

    bump rufi to v0.5.0

--- Formula/rufi.rb
+++ Formula/rufi.rb
@@ -1,17 +1,17 @@
 class Rufi < Formula
-    desc "A fast application launcher for macOS"
-    homepage "https://github.com/darwin808/rufi"
-    url "https://github.com/darwin808/rufi/releases/download/v0.4.0/rufi-macos.tar.gz"
-    sha256 "a30b53afe298b5e64eb2f52ff0963204f80f1b31db7046997c5de0cbfc56e5e5"
-    license "MIT"
+  desc "A fast application launcher for macOS"
+  homepage "https://github.com/darwin808/rufi"
+  url "https://github.com/darwin808/rufi/releases/download/v0.5.0/rufi-macos.tar.gz"
+  sha256 "2a60623119c7e29ce397c9964c5d12cd901065af4793b38cd425d5a331539793"
+  license "MIT"
 
-    depends_on :macos
+  depends_on :macos
 
-    def install
-      bin.install "rufi"
-    end
-
-    test do
-      assert_match "rufi", shell_output("#{bin}/rufi --help")
-    end
+  def install
+    bin.install "rufi"
   end
+
+  test do
+    assert_match "rufi", shell_output("#{bin}/rufi --help 2>&1", 1)
+  end
+end
